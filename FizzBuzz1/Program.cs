e1c24a3ff77d35ec0c2f874a7aa04b45088fa0b8
Dustin Fox <djf149@hotmail.com>
2021-05-02 17:57:26 -0400

    first commit

---
+++ FizzBuzz1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fizz if mod of 5.
+            //buzz if mod of 3.
+            //FizzBuzz if both are mod of 5 and 3
+
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                } else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}");
+                }
+
+              
+            }
+        }
+    }
+}
